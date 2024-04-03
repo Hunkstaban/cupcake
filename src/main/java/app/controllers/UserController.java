f90466f04b6f87e8a874181f3ff0d51f064b9ac5
vaangis93 <vaangis93@gmail.com>
2024-04-03 11:05:20 +0200

    made method :  route, login, logout in class="UserController"

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,18 +1,47 @@
 package app.controllers;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+
+import static app.persistence.UserMapper.login;
 
 public class UserController {
 
 
-    public static void addRoute (Javalin app, ConnectionPool connectionPool) {
+    public static void addRoute(Javalin app, ConnectionPool connectionPool) {
 
-app.post()
+        app.post("login", ctx -> login(ctx, connectionPool));
+        app.get("logout", ctx -> logout(ctx));
+
 
     }
 
+    private static void logout(Context ctx) {
+        ctx.req().getSession().invalidate();
+        ctx.render("index.html");
+    }
 
+    private static void login(Context ctx, ConnectionPool connectionPool) {
+
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
+
+        try {
+            User user = UserMapper.login(email, password, connectionPool);
+            ctx.sessionAttribute("currentUser : ", user);
+            ctx.render("index.html");
+
+        } catch (DatabaseException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("index.html");
+        }
+    }
 
 
 }// ---------------------------------  end class ------------------------------------
