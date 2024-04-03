349bc4207badd87e7de896cf6f347ebd8d730154
vaangis93 <vaangis93@gmail.com>
2024-04-03 11:32:47 +0200

    controller needs to be checked

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,8 +1,10 @@
 package app.controllers;
 
+import app.entities.Order;
 import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.CupcakeMapper;
 import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -21,7 +23,7 @@ public class UserController {
 
 
     }
-
+// .invalidate = we dont need it anymore. and want to close it.
     private static void logout(Context ctx) {
         ctx.req().getSession().invalidate();
         ctx.render("index.html");
@@ -33,10 +35,15 @@ public class UserController {
         String password = ctx.formParam("password");
 
         try {
+            // TODO ask that boys = sessionAtrribute hele page??
+
             User user = UserMapper.login(email, password, connectionPool);
             ctx.sessionAttribute("currentUser : ", user);
             ctx.render("index.html");
+            List<Order> cartList = CupcakeMapper.getCart() // TODO what we do here + make getcart method ??
+            ctx.attribute("cart : ", cartList);
 
+            // TODO ask that boys = Atrribute er den ene command ??
         } catch (DatabaseException e) {
             ctx.attribute("message", e.getMessage());
             ctx.render("index.html");
