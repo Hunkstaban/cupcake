72a5920a2d48b20f8cdce3459fc98d929843a829
petert <peter.torgersen@gmail.com>
2024-04-03 10:50:54 +0200

    Added DB connection variables in Main class.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,18 @@
 package app;
 
 import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args) {
 
         // Initializing Javalin and Jetty webserver
@@ -14,6 +22,6 @@ public class Main {
         }).start(7070);
 
         // Routing
-        app.get("/", ctx ->  ctx.render("index"));
+        app.get("/", ctx ->  ctx.render("login"));
     }
 }
---
+++ src/main/java/app/persistence/OrderDetailMapper.java
@@ -0,0 +1,7 @@
+package app.persistence;
+
+public class OrderDetailMapper {
+
+
+
+}
