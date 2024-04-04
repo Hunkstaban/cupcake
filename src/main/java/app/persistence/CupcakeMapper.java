747450420c02f05413241c3fb69e79cf578117a4
Oliver <olivergud@gmail.com>
2024-04-04 11:52:15 +0200

    made getAllToppings and getAllBases in CupcakeMapper

--- src/main/java/app/persistence/CupcakeMapper.java
+++ src/main/java/app/persistence/CupcakeMapper.java
@@ -59,6 +59,53 @@ public class CupcakeMapper {
 
     }
 
+    public static List<Topping> getAllToppings(ConnectionPool connectionPool) {
+        String sql = "SELECT * FROM toppings";
+        List<Topping> toppingList = new ArrayList<>();
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int toppingID = rs.getInt("topping_id");
+                String toppingName = rs.getString("topping_name");
+                int toppingPrice = rs.getInt("topping_price");
+                toppingList.add(new Topping(toppingID, toppingName, toppingPrice));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return toppingList;
+
+    }
+
+    public static List<Base> getAllBases(ConnectionPool connectionPool) {
+        String sql = "SELECT * FROM bases";
+        List<Base> baseList = new ArrayList<>();
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int baseID = rs.getInt("base_id");
+                String baseName = rs.getString("base_name");
+                int basePrice = rs.getInt("base_price");
+                baseList.add(new Base(baseID, baseName, basePrice));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return baseList;
+    }
+
+
+
 
 
 }
